bd552417d99b3c6b9120a1d9aa924c549a4eef77
ojpascale <sylviapascale@yahoo.com>
2023-11-15 12:53:07 -0600

    Modified main.tf file, created .gitignore file

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ provider "aws" {
 resource "aws_instance" "ovia-instance" {
   ami                     = "ami-0e83be366243f524a"
   instance_type           = "t2.micro"
+  tags = {
+    Name = "ovia-ubuntu"
+  }
+}
