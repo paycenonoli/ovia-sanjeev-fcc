82711a489310fec82898521537468b3ce1493b61
ojpascale <sylviapascale@yahoo.com>
2023-11-15 10:32:52 -0600

    First commit for Terraform Project

---
+++ main.tf
@@ -0,0 +1,9 @@
+# Define provider
+provider "aws" {
+  region  = "us-east-2"
+  profile = "default"
+}
+#Create an EC2 instance
+resource "aws_instance" "ovia-instance" {
+  ami                     = "ami-0e83be366243f524a"
+  instance_type           = "t2.micro"
