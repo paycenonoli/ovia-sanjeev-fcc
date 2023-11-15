0aa7268f07c373d0dd11a76519f06936f11209b1
ojpascale <sylviapascale@yahoo.com>
2023-11-15 13:37:43 -0600

    Modified main.tf file: created a vpc and subnet

--- main.tf
+++ main.tf
@@ -3,11 +3,29 @@ provider "aws" {
   region  = "us-east-2"
   profile = "default"
 }
-#Create an EC2 instance
-resource "aws_instance" "ovia-instance" {
-  ami                     = "ami-0e83be366243f524a"
-  instance_type           = "t2.micro"
+
+# Create a VPC
+resource "aws_vpc" "ovia-vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "ovia-ubuntu"
+    Name = "production"
   }
 }
+# Create subnets
+resource "aws_subnet" "ovia-subnet" {
+  vpc_id     = aws_vpc.ovia-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
+
+# #Create an EC2 instance
+# resource "aws_instance" "ovia-instance" {
+#   ami                     = "ami-0e83be366243f524a"
+#   instance_type           = "t2.micro"
+#   tags = {
+#     Name = "ovia-ubuntu"
+#   }
+# }
